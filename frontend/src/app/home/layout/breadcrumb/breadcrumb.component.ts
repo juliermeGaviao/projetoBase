aad5976c0120797e94fcbc642fe10f172b0d2193
julierme.gaviao <julierme.gaviao@jointecnologia.com.br>
2024-12-10 20:34:50 -0300

    Nova migalha de pão.

--- frontend/src/app/home/layout/breadcrumb/breadcrumb.component.ts
+++ frontend/src/app/home/layout/breadcrumb/breadcrumb.component.ts
@@ -1,22 +1,75 @@
-import { Component, OnInit } from '@angular/core';
-import { BreadcrumbService } from '../../../services/breadcrumb.service';
+import { AfterViewInit, ChangeDetectorRef, Component } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter, startWith } from 'rxjs';
+
+interface Breadcrumb {
+  label: string;
+  url: string;
+  visible: boolean;
+}
 
 @Component({
   selector: 'app-breadcrumb',
-  template: `
-    <br-breadcrumb *ngIf="showBreadcrumb" label="Breadcrumb" [links]="links"></br-breadcrumb>
-  `,
+  templateUrl: './breadcrumb.component.html'
 })
-export class BreadcrumbComponent implements OnInit {
-  showBreadcrumb = false;
-  links: { label: string; url: string; home?: boolean }[] = [];
+export class BreadcrumbComponent implements AfterViewInit {
+  links: Breadcrumb[] = [];
 
-  constructor(private readonly breadcrumbService: BreadcrumbService) {}
+  constructor(
+    private readonly activatedRoute: ActivatedRoute,
+    private readonly router: Router,
+    private readonly cdr: ChangeDetectorRef  // Injetar ChangeDetectorRef
+  ) { }
 
-  ngOnInit(): void {
-    this.breadcrumbService.breadcrumb$.subscribe((links) => {
-      this.links = links;
-      this.showBreadcrumb = this.links.length > 1;
+  ngAfterViewInit(): void {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      startWith(new NavigationEnd(0, this.router.url, this.router.url)) // Emite um evento inicial de NavigationEnd
+    ).subscribe(() => {
+      const initialLinks = [
+        {
+          label: 'Página Inicial',
+          url: '/',
+          home: true,
+        }
+      ];
+
+      this.links = this.buildBreadcrumbs(this.activatedRoute.root, '', initialLinks);
+
+      // Forçar detecção de mudanças
+      this.cdr.detectChanges();
     });
   }
+
+  buildBreadcrumbs(route: ActivatedRoute, url: string = '', breadcrumbs: any[] = []): Breadcrumb[] {
+    const children: ActivatedRoute[] = route.children;
+
+    if (children.length === 0) {
+      return breadcrumbs;
+    }
+
+    for (let child of children) {
+      const routeURL: string = child.snapshot.url.map(segment => segment.path).join('/');
+
+      if (routeURL !== '') {
+        url += `/${routeURL}`;
+        const breadcrumb: Breadcrumb = {
+          label: child.snapshot.data['breadCrumb'] || routeURL,
+          url,
+          visible: true,
+        };
+
+        if (breadcrumb.label === 'home') {
+          breadcrumb.label = 'Página Inicial';
+        }
+
+        breadcrumbs.push(breadcrumb);
+      }
+
+      return this.buildBreadcrumbs(child, url, breadcrumbs);
+    }
+
+    return breadcrumbs;
+  }
+
 }
