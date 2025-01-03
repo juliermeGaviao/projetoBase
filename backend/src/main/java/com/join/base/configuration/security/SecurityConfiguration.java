812749c15a8f7f889c06aecd1b2ad4e4ca466864
julierme.gaviao <julierme.gaviao@jointecnologia.com.br>
2025-01-02 21:06:59 -0300

    Realocação da filtragem de endpoints no backend conforme usuário logado.

--- backend/src/main/java/com/join/base/configuration/security/SecurityConfiguration.java
+++ backend/src/main/java/com/join/base/configuration/security/SecurityConfiguration.java
@@ -48,8 +48,7 @@ public class SecurityConfiguration {
 			httpSecurity.addFilterBefore(new AuthFilter(tokenValidator), UsernamePasswordAuthenticationFilter.class)
 
 			// define os paths autorizados a excutar sem autenticação e os path necessários autenticação
-			//.authorizeHttpRequests(req -> req.requestMatchers("/sso/**").permitAll().anyRequest().authenticated());
-			.authorizeHttpRequests(req -> req.anyRequest().permitAll());
+			.authorizeHttpRequests(req -> req.requestMatchers("/sso/**").permitAll().anyRequest().authenticated());
 		}
 
         httpSecurity.cors(c -> c.configurationSource(this::getCorsConfiguration));
